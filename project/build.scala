3a9969ab591d4665d621033a2091d553929c5c8c
Matt Fellows <matt.fellows@onegeek.com.au>
2014-09-23 18:55:24 +1000

    Merged feature/pretty-print for pretty JSON formatting when asked\!

--- project/build.scala
+++ project/build.scala
@@ -11,7 +11,7 @@ import scoverage.ScoverageSbtPlugin.instrumentSettings
 object RespiteBuild extends Build {
   val Organization = "au.com.onegeek"
   val Name = "Respite REST Framework"
-  val Version = "0.2.1"
+  val Version = "0.2.2"
 
   lazy val core = Project (
     "respite-core",
